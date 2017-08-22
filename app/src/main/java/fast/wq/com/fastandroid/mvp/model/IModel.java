6bbc60dbc878a74b0c54cfc71ee4b14ea2c13395
wangq <1281550584@qq.com>
2017-08-22 08:18:49 +0800

    mvp

---
+++ app/src/main/java/fast/wq/com/fastandroid/mvp/model/IModel.java
@@ -0,0 +1,8 @@
+package fast.wq.com.fastandroid.mvp.model;
+
+/**
+ * Created by wangqiang on 2017/8/22.
+ */
+
+public class IModel {
+}
---
+++ app/src/main/java/fast/wq/com/fastandroid/mvp/view/IViewInterface.java
@@ -0,0 +1,11 @@
+package fast.wq.com.fastandroid.mvp.view;
+
+/**
+ * Created by wangqiang on 2017/8/22.
+ */
+
+public interface IViewInterface {
+    public void showDatas();
+    public void showLoading();
+    public void hideLoading();
+}
