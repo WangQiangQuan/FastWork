58611a2f13ccb5d30aa36049e16ba6b89530a8dd
Wangqiang <qiang.wang@dewmobile.net>
2017-08-25 18:38:36 +0800

    zhan

---
+++ app/src/main/java/fast/wq/com/fastandroid/algorithm/stackUtils.java
@@ -0,0 +1,14 @@
+package fast.wq.com.fastandroid.algorithm;
+
+/**
+ * 堆栈工具
+ */
+public class stackUtils {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+}
