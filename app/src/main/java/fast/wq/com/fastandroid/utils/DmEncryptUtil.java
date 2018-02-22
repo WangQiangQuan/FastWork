390ccc80a8ef7e883ac6589e65a817ae004c3931
Wangqiang <qiang.wang@dewmobile.net>
2018-02-22 18:56:05 +0800

    tijiao

---
+++ app/src/main/java/fast/wq/com/fastandroid/utils/DmEncryptUtil.java
@@ -0,0 +1,9 @@
+package fast.wq.com.fastandroid.utils;
+
+/**
+ * http://blog.csdn.net/sinat_29581293/article/details/52126519
+ * https://www.cnblogs.com/stay-9527/p/3665668.html
+ */
+
+public class DmEncryptUtil {
+}
