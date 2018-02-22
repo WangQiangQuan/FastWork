cd251497a4fdedcba5105b9b7edc8c2a1dbaa8c9
Wangqiang <qiang.wang@dewmobile.net>
2018-02-22 10:14:36 +0800

    glide 相关功能提交

---
+++ app/src/main/java/fast/wq/com/fastandroid/glide/progress/ProgressInterceptor.java
@@ -0,0 +1,32 @@
+package fast.wq.com.fastandroid.glide.progress;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by admin on 2018/2/13.
+ */
+
+public class ProgressInterceptor implements Interceptor{
+    static final Map<String, ProgressListener> LISTENER_MAP = new HashMap<>();
+
+    public static void addListener(String url, ProgressListener listener) {
+        LISTENER_MAP.put(url, listener);
+    }
+
+    public static void removeListener(String url) {
+        LISTENER_MAP.remove(url);
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        Response response = chain.proceed(request);
+        return response;
+    }
+}
