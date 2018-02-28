9665ddf591a40d25fc3a795e499884910d8987e0
wangq <1281550584@qq.com>
2018-02-28 21:21:23 +0800

    blockingququ

---
+++ app/src/main/java/fast/wq/com/fastandroid/thread/syn/BlockingTest.java
@@ -0,0 +1,70 @@
+package fast.wq.com.fastandroid.thread.syn;
+
+import android.util.Log;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+
+/**
+ *
+ */
+
+public class BlockingTest {
+    private static final String TAG = "BlockingTest";
+
+    public void testMain() {
+        ArrayBlockingQueue<String> mbq = new ArrayBlockingQueue<>(1);
+        //启动三个生产者线程
+        new Producter(mbq).start();
+        new Producter(mbq).start();
+        new Producter(mbq).start();
+        //启动一个消费者线程
+        new Consumer(mbq).start();
+    }
+
+    class Producter extends Thread {
+        private ArrayBlockingQueue<String> bq;
+
+
+        public Producter(ArrayBlockingQueue<String> bq) {
+            this.bq = bq;
+        }
+
+        @Override
+        public void run() {
+            String[] arry = new String[]{"java", "thread", "bq"};
+            for (int i = 0; i < 999999; i++)
+                Log.i(TAG, "run: 生产者准备生产者元素");
+            String value = arry[i % 3];
+            try {
+                Thread.sleep(200);
+                bq.put(value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.i(TAG, "run: " + getName() + "生产完成" + bq);
+        }
+    }
+
+
+    class Consumer extends Thread {
+        private ArrayBlockingQueue<String> bq;
+
+        public Consumer(ArrayBlockingQueue<String> bq) {
+            this.bq = bq;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                Log.i(TAG, "run: 消费者准备消费集合元素");
+                try {
+                    Thread.sleep(200);
+                    bq.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
